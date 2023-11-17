471452bc7904a21b68bf36ad3f22eb2f05457bb3
Amal221200 <amalmurikkoli2000@gmail.com>
2023-11-17 21:07:14 +0530

    navbar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,14 @@
+import Navbar from "@/components/Navbar";
 import authOptions from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
 const HomePage = async () => {
-    const session = await getServerSession(authOptions);
-    return (<h1>hello {session?.user?.name}</h1>);
+    // const session = await getServerSession(authOptions);
+    return (
+        <>
+            <Navbar />
+        </>
+    );
 }
 
 export default HomePage;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { Bell, Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface navLinkProps {
+    name: string,
+    href: string
+}
+
+const navLinks: navLinkProps[] = [
+    {
+        name: 'Home',
+        href: '/home'
+    },
+
+    {
+        name: 'TV Shows',
+        href: '/home/shows'
+    },
+    {
+        name: 'Movies',
+        href: '/home/movies'
+    },
+    {
+        name: 'Recently Added',
+        href: '/home/recently'
+    },
+    {
+        name: 'My Lists',
+        href: '/home/user/lists'
+    }
+]
+
+const Navbar = () => {
+    const pathname = usePathname()
+    return (
+        <nav className="w-full max-w-7xl mx-auto flex justify-between items-center px-5 sm:px-6 py-5 lg:py-8">
+            <div className="flex items-center">
+                <Link href="/home" className="w-32">
+                    <Image src="/netflix_logo.svg" alt="logo" width={128} height={50} priority className="" />
+                </Link>
+                <ul className="lg:flex hidden gap-x-4 ml-14">
+                    {
+                        navLinks.map(navLink => (
+                            <li key={navLink.href}>
+                                <Link href={navLink.href} className={cn("text-gray-300 text-sm font-semibold", navLink.href === pathname && "text-white font-bold underline")}>
+                                    {navLink.name}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+
+            <div className="flex items-center gap-x-8">
+                <Search className="w-5 h-5 text-gray-300 cursor-pointer" />
+                <Bell className="w-5 h-5 text-gray-300 cursor-pointer" />
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
