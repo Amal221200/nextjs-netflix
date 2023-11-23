0c25fab6ffb744930ec023898a51fbe5fc02be89
Amal221200 <amalmurikkoli2000@gmail.com>
2023-11-23 10:52:50 -0800

    remove buttond

--- src/components/custom/buttons/SeeMoreButton.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { Button } from "@/components/ui/button";
-
-const SeeMoreButton = () => {
-    return ( <Button type="button">See More</Button> );
-}
- 
-export default SeeMoreButton;
